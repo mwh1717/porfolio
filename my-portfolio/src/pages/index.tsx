6b45766eaeb983b51438ebba093fdc5c0ca696b6
mwh1717 <michael.wells.howell@gmail.com>
2023-04-02 17:04:41 -0400

    first home section complete

--- my-portfolio/src/pages/index.tsx
+++ my-portfolio/src/pages/index.tsx
@@ -7,15 +7,70 @@ export default function Home() {
       <div className={styles.wrapper1}>
         <div className={styles.picture}>
           <Image alt="image of Michael W Howell and his wife at wedding"
-            width='300'
-            height='450'
+            width={500}
+            height={650}
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
             src='/m&k.JPG'
             className={styles.photo}
           />
         </div>
         <div className={styles.gradings}>
-
+          <div className={styles.techWrapperSingle}>
+            <div className={styles.gradingsTitle}>Mastered</div>
+            <Image alt="html css and javascript logos"
+              width={500}
+              height={300}
+              style={{
+                width: 'auto',
+                height: '100px',
+              }}
+              src='/html5-logo-31816.png'
+              className={styles.photo}
+            />
+          </div>
+          <div className={styles.techWrapperDouble}>
+            <div className={styles.gradingsTitle}>Aspiring</div>
+            <div className={styles.techWrapperDoubleImages}>
+            <Image alt="tailwind logo"
+                width={100}
+                height={100}
+                style={{
+                  width: 'auto',
+                  height: '75px',
+                  position: 'relative',
+                  top: '12px'
+                }}
+                src='/tailwind-css.svg'
+                className={styles.photo}
+              />
+              <Image alt="java logo"
+                width={100}
+                height={100}
+                style={{
+                  width: 'auto',
+                  height: '100px',
+                }}
+                src='/Java_programming_language_logo.svg'
+                className={styles.photo}
+              />
+              <Image alt="react logo"
+                width={100}
+                height={100}
+                style={{
+                  width: 'auto',
+                  height: '100px',
+                }}
+                src='/react-logo.png'
+                className={styles.photo}
+              />
+            </div>
+          </div>
         </div>
+      </div>
+      <div className={styles.wrapper2}>
       </div>
     </div>
   </>
