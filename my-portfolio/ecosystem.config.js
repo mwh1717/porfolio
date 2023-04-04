45d7b52564bd08ec1b021945b6ec7f3368af23ce
mwh1717 <michael.wells.howell@gmail.com>
2023-04-03 23:35:01 -0400

    deploy files

---
+++ my-portfolio/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'michaelwhowell.dev',
+      script: './script.js',
+      env: {
+        HOST: 'localhost',
+        PORT: 3000
+      }
+    }
+  ]
+}
