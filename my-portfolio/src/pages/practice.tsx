95c69e690cea4b5ebcf254b842182f2e01f05dd1
mwh1717 <michael.wells.howell@gmail.com>
2023-04-08 20:11:10 -0400

    added dnd app

--- my-portfolio/src/pages/practice.tsx
+++ my-portfolio/src/pages/practice.tsx
@@ -3,6 +3,7 @@ import InputSorter from '@/components/InputSorter';
 import TextInputValidation from '@/components/TextInputValidation';
 import NestingTree from '@/components/NestingTree';
 import styles from '@/styles/Practice.module.css';
+import Image from 'next/image';
 
 export default function Practice() {
   return (
@@ -11,6 +12,24 @@ export default function Practice() {
       <InputSorter />
       <TextInputValidation />
       <NestingTree />
+      <h2 className={styles.projectDivTitle}>Practice Apps</h2>
+      <div className={styles.appDiv}>
+        <a className={styles.link} href='https://dnd5eshopkeeper.netlify.app/'>
+          <Image alt="screenshot of the dnd5e shopkeeper app"
+            width={500}
+            height={650}
+            style={{
+              width: "80%",
+              height: "auto",
+            }}
+            src="/shopkeep.jpeg"
+            className={styles.photo}
+          />
+        </a>
+        <div className={styles.appDesc}>
+          This is my DnD 5e Shopkeeper application, created with NextJS with Typescript and Tailwind. When the user lands on the page initially, an API call is made to the free dnd5eapi to gather all of the 5e magic items. Six random items from that list are selected and an API call is made for each to gather the item's specific information. Each item is then rendered within a card that has a different border color depending on the item's rarity. The user can choose to purchase the item, which removes the card from the DOM. More features will be built out at a later time for continued practice.
+        </div>
+      </div>
     </div>
   )
 }
